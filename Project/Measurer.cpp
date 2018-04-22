3d83b15e8c0ddf732d3851cf501494295b309b56
范志康 <goodman.capu@gmail.com>
2018-04-22 15:05:13 -0700

    Add mean and std output

--- Project/Measurer.cpp
+++ Project/Measurer.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <numeric>
+#include <math.h>
 #include <unistd.h>
 
 using namespace std;
@@ -19,19 +21,26 @@ static string baseDir = "/Users/Frank/Documents/Code/CSE 221/Project/Project/Res
 class Measurer {
 public:
     static void measure(double (*func)(), string name, int repeat = 20) {
-        cout << "Measurement " << name << " begins" << endl;
+        cout << "Measure " << name << " begins" << endl;
         
         fstream file;
         file.open(baseDir + name + ".txt", ios::out);
         vector<double> results;
-        for (int r = 1; r <= repeat; r++) {
+        for (int r = 1; r <= repeat; ++r) {
             double result = func();
             results.push_back(result);
             file << r << '\t' << result << endl;
             // Random sleep for 1 ~ 3 second
             usleep((1 + arc4random() % 3) * 1000000);
         }
+        double mean = accumulate(results.begin(), results.end(), 0.0) / results.size();
+        double var = 0.0;
+        for_each(results.begin(), results.end(), [&](const double val) {
+            var += pow((val - mean), 2);
+        });
+        double std = sqrt(var / (results.size() - 1));
+        file << endl << "Mean: " << mean << " Std: " << std << endl;
         file.close();
-        cout << "Measurement " << name << " completes" << endl;
+        cout << "Measure " << name << " completes" << endl;
     }
 };
