096f62fc11387ec163c5d5de672cc10115f5b950
范志康 <goodman.capu@gmail.com>
2018-04-22 13:50:34 -0700

    Initial Commit

---
+++ Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by 范志康 on 2018/4/22.
+//  Copyright © 2018年 范志康. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
