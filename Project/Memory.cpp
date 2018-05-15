e1b58d4f75173597a14c89021aa1e74bfdd6b63d
Frank <goodman.capu@gmail.com>
2018-05-15 13:54:44 -0700

    Page fault?

--- Project/Memory.cpp
+++ Project/Memory.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "Measurer.hpp"
+#include <fcntl.h>
 #include <sys/mman.h>
 
 static int mem_loop = 1e6;
@@ -14,13 +15,13 @@ static int mem_loop = 1e6;
 class Memory {
 public:
     static void measure_all() {
-        vector<int> sizes;
-        for (int i = 12; i < 26; i++) {
-            sizes.push_back(pow(2, i));
-        }
-        Measurer::measure_multi(memory_access_latency, sizes, "Memory Access", "Array Size", "Latency");
-        Measurer::measure(memory_read_bandwidth, "Memory Read", "Bandwidth");
-        Measurer::measure(memory_write_bandwidth, "Memory Write", "Bandwidth");
+//        vector<int> sizes;
+//        for (int i = 12; i < 26; i++) {
+//            sizes.push_back(pow(2, i));
+//        }
+//        Measurer::measure_multi(memory_access_latency, sizes, "Memory Access", "Array Size", "Latency");
+//        Measurer::measure(memory_read_bandwidth, "Memory Read", "Bandwidth");
+//        Measurer::measure(memory_write_bandwidth, "Memory Write", "Bandwidth");
         Measurer::measure(page_fault_time, "Page Fault", "Time");
     }
     
@@ -50,7 +51,7 @@ private:
         long repeat = total_size / read_size;
         char *data = (char *)malloc(total_size);
         char *buffer = (char *)malloc(read_size);
-        memset(data, 0, total_size);
+        memset(data, 1, total_size);
         memset(buffer, 0, read_size);
         start = rdtsc();
         for (int i = 0; i < repeat; i++) {
@@ -78,6 +79,26 @@ private:
     }
     
     static double page_fault_time() {
-        return 0;
+        uint64_t start, end;
+        
+        size_t total_size = (unsigned long)pow(2, 30) * 20, read_size = 4 * 1024;
+        long repeat = 5;
+        size_t stride = total_size / repeat;
+        char *data = (char *)malloc(total_size);
+        char *buffer = (char *)malloc(read_size);
+        memset(data, 0, total_size);
+        memset(buffer, 0, read_size);
+        for (int i = 0; i < repeat; i++) {
+            memset(data + i * stride, 1, read_size);
+        }
+        start = rdtsc();
+        for (int i = 0; i < repeat; i++) {
+            memcpy(buffer, data + i * stride, read_size);
+        }
+        end = rdtsc();
+        free(data);
+        free(buffer);
+        
+        return total_size / ((double)(end - start) * repeat);
     }
 };
